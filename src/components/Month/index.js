74cc9d38340b5b2489f564ff59a2ed530ea82f8a
vivianachoque <vivianaachoque@gmail.com>
2021-01-19 18:38:46 -0300

    hago comoponente mes y paso fecha de hoy

--- src/components/Month/index.js
+++ src/components/Month/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
+import styles from './index.module.scss';
 import Days from '../Days';
 
 export default function Month({ name, days }) {
+  const allDays = [...Array(days - 1).keys()];
   return (
     <div>
-      
+      <h1>{ name }</h1>
+      <div className={styles.containerDays}>
+         {
+            allDays.map((el) => {
+               <Days day={el + 1} />
+            })
+         }  
+      </div>
     </div>
   )
 }
